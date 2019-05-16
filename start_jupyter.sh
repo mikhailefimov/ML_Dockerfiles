9b38e582ae694db8195d2be88413c8ee7c98a104
Mikhail Efimov <mikhail.m.efimov@gmail.com>
2019-05-16 12:38:58 +0300

    Start notebook in safer way

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 JUPYTER_PORT=8888
-(sleep 5 && xdg-open http://127.0.0.1:${JUPYTER_PORT}) &
-docker run --runtime nvidia --rm -it -p ${JUPYTER_PORT}:8888  -v $(pwd):/notebooks emm/jupyter
+TOKEN=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 48)
+(sleep 5 && xdg-open http://127.0.0.1:${JUPYTER_PORT}?token=${TOKEN}) &
+docker run --runtime nvidia --rm -it -p ${JUPYTER_PORT}:8888  -v $(pwd):/notebooks emm/jupyter jupyter notebook --notebook-dir=/notebooks --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token="'${TOKEN}'"
