a2c82d64b8e30390401811d56f7c69d5527484c7
Mikhail Efimov <mikhail.m.efimov@gmail.com>
2019-05-17 01:02:25 +0300

    Rebase Jupyter Dockerfile on image jupyter/scipy-notebook from docker-stacks

--- build.4.jupyter.sh
+++ build.4.jupyter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp wheels/*.whl jupyter
+chmod 777 jupyter/*.whl
 docker build --rm -t emm/jupyter ./jupyter
 rm jupyter/*.whl
--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,4 +2,4 @@
 JUPYTER_PORT=8888
 TOKEN=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 48)
 (sleep 5 && xdg-open http://127.0.0.1:${JUPYTER_PORT}?token=${TOKEN}) &
-docker run --runtime nvidia --rm -it -p ${JUPYTER_PORT}:8888  -v $(pwd):/notebooks emm/jupyter jupyter notebook --notebook-dir=/notebooks --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token="'${TOKEN}'"
+docker run --runtime nvidia --rm -it -p ${JUPYTER_PORT}:8888 --user "$(id -u)" --group-add users -v $(pwd):/home/jovyan emm/jupyter  start-notebook.sh  --no-browser --NotebookApp.token="'${TOKEN}'"
